67ff4204f19c0e4433928515481d59a89ee8c966
Deblyn Prado <deblynprado@hotmail.com>
2011-08-05 01:05:49 -0300

    Adicionados efeitos CSS3 e realizadas correcoes nas paginas

--- enviar.php
+++ enviar.php
@@ -1,25 +1,25 @@
 ﻿<?php
+	/* Verifica qual éo sistema operacional do servidor para ajustar o cabeçalho de forma correta.  */
+	if(PATH_SEPARATOR == ";") $quebra_linha = "\r\n"; //Se for Windows
+	else $quebra_linha = "\n"; //Se "nÃ£o for Windows"
+	
 	$nome = $_POST['nome'];
-	$email = $_POST['email'];
+	$remetente = 'contato@deblyn.com.br';
+	$email = $_POS['email'];
 	$assunto = $_POST['assunto'];
 	$mensagem = $_POST['mensagem'];
+	$destinatario = 'deblynprado@gmail.com'; 
+  	
+	$mensagemHTML = '<p><b><i>'.$mensagem.'</i></b></p>';
 	
-	$para = 'deblynprado@gmail.com'; 
-	$assunto = '$assunto';
-	$cabecalho = "MIME-Version: 1.0" . "\r\n".
-				 "Content-type: text/html; charset=iso-8859-1" . "\r\n".
-			   	 "From: $email" . "\r\n" .
-				 "Reply-To: deblyn@deblyn.com";
-
-	$corpoDoEmail = $nome . "<br />";
-	$corpoDoEmail .= $email . "<br />";
-	$corpoDoEmail .= $assunto . "<br /><br />";
-	$corpoDoEmail .= $mensagem;
-			 
-	$status = mail($para, $assunto, $corpoDoEmail, $cabecalho);
+	$headers = "MIME-Version: 1.1" .$quebra_linha;
+	$headers .= "Content-type: text/html; charset=iso-8859-1" .$quebra_linha;
+	$headers .= "From: " . $destinatario.$quebra_linha;
+	$headers .= "Reply-To: " . $remetente . $quebra_linha;
 	
-	if($status)
-		echo "<script> alert('Mensagem enviada com sucesso!.'); </script>";
-	else
-		echo "<script> alert('A Mensagem não foi enviada, por favor tente novamente!'); </script>";
+	if(!mail($destinatario, $assunto, $mensagemHTML, $headers ,"-r".$destinatario)){ // Se for Postfix
+    $headers .= "Return-Path: " . $destinatario . $quebra_linha; // Se "não for Postfix"
+    mail($destinatario, $assunto, $mensagemHTML, $headers );
+	}
+	
 ?>
